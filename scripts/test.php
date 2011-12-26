71a44bbf5ff9d6746bf4c2350dcffb21d6fa96e5
Bojan Ilievski <drakuwa@gmail.com>
2011-12-26 04:38:04 +0100

    updated the web service script

--- scripts/test.php
+++ scripts/test.php
@@ -5,40 +5,59 @@ $content = $_POST['content'];
 $content.="\n";
 
 if($command=='get'){
-	$myFile = "text";
-	$fh = fopen($myFile, 'r');
-	$theData = fread($fh, filesize($myFile));
+	$collectionFile = "text";
+	$fh = fopen($collectionFile, 'r');
+	$theData = fread($fh, filesize($collectionFile));
 	fclose($fh);
 
-	$myFile2 = "counter";
-        $fh3 = fopen($myFile2, 'r');
-        $theData2 = fread($fh3, filesize($myFile2));
+	$counterFile = "counter";
+        $fh3 = fopen($counterFile, 'r');
+        $version = fread($fh3, filesize($counterFile));
         fclose($fh3);
 
-	if($theData2==$content){
+	if($version==$content){
 		echo 'same version';
 	}
 	else {
-		echo 'version: '.$theData2;
+		echo 'version: '.$version;
 		echo $theData;
 	}
 }
 elseif($command=='add'){
-	$myFile = "text";
-	$fh = fopen($myFile, 'a') or die("can't open file");
-	fwrite($fh, $content);
-	fclose($fh);
+	$exists = 0;
+	$collectionFile = "text";
+	$fh = fopen($collectionFile, 'a') or die("can't open file");
+	$fhTmp = fopen($collectionFile, "r") or die("can't open file for reading");
 
-	$counterFile = "counter";
-	$fh1 = fopen($counterFile, 'r');
-	$count = fread($fh1, filesize($counterFile));
-	$count = $count + 1;
+	if($fhTmp){
+		while (($buffer = fgets($fhTmp)) !== false) {
+		if($content==$buffer) $exists=1;
+    		}
+    		if (!feof($fhTmp)) {
+        		echo "Error: unexpected fgets() fail\n";
+    		}
+		fclose($fhTmp);
+	}
 
-	$fh2 = fopen($counterFile, 'w') or die("can't open file");
-	fwrite($fh2, $count."\n");
-	fclose($fh1);
-	fclose($fh2);
-	echo 'Success!';
+	if($exists){
+		echo "AP already exists!\n";
+	}
+	else {
+		fwrite($fh, $content);
+		fclose($fh);
+
+		$counterFile = "counter";
+		$fh1 = fopen($counterFile, 'r');
+		$count = fread($fh1, filesize($counterFile));
+		fclose($fh1);
+
+		$count = $count + 1;
+
+		$fh2 = fopen($counterFile, 'w') or die("can't open file");
+		fwrite($fh2, $count."\n");
+		fclose($fh2);
+		echo 'Success!';
+	}
 }
 else{
 	echo "Operation Failed!";
